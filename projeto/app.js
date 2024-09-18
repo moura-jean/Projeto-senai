349b76f35da5c47df30a07b48f4b4dc04087c327
Bowie <jean.karlos72@gmail.com>
2024-09-17 22:11:21 -0300

    segunda versao do codigo

--- projeto/app.js
+++ projeto/app.js
@@ -1,12 +1,28 @@
 const express = require("express")
 const app = express()
 const port = 8080
+const path = require('path')
 
 
-app.get('/sobre', (req, res) => {
-    res.send('<h1>Aula 16/09 </h1>')
+app.get('/api-tester', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public/index.html'));
+});
+
+app.get('/', (req, res) => {
+    res.send('Funcionou essa p****!')
 })
 
 app.listen(port,() =>{
     console.log('Running')
 })
+
+app.use(express.json()); // Para permitir o parsing de JSON no corpo das requisições
+
+
+app.post('/rota', (req, res) => {
+    const dados = req.body
+    console.log(dados)
+    // Processar os dados recebidos
+    res.send(`Dados recebidos: ${JSON.stringify(dados)}`)
+});
+    
